2e8c9dbd908c89f47db57e52999fe997ff509de7
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2021-11-10 21:32:38 +0330

    Session-6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from browser import document, alert, console
+from browser import document, alert, console, ajax
 from browser.template import Template
 
 
@@ -23,3 +23,25 @@ document['text'].bind('input', show_output)
 # Template Variable
 Template(document['hi-to']).render(name="Mohammad")
 Template(document['sum']).render(a=10, b=5)
+
+
+# Ajax call
+url = "https://api.chucknorris.io/jokes/random"
+
+
+def on_complete(req):
+    import json
+    data = json.loads(req.responseText)
+    joke = data['value']
+    document['show-joke'].textContent = joke
+
+
+def get_joke(e):
+    req = ajax.ajax()
+    req.open('GET', url, True)
+    req.send()
+    document['show-joke'].textContent = "LOADING....."
+    req.bind('complete', on_complete)
+
+
+document['joke-btn'].bind('click', get_joke)
